27f777fb24da625aaf731b8d768832a19749e156
nadayehia115 <nadafawzy115@gmail.com>
2026-03-24 14:24:42 +0200

    Update HelloWorld.cs

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("hello");
+    }
+    void Update()
+    {
+        Debug.Log("worlddd");
+    }
+}
